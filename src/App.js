20e5f6c4b834ea27fe3899701e72f2cdcf79804c
Haoran Zhou <kens0serious@gmail.com>
2022-02-01 00:35:32 -0500

    created context

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Submenu from './Submenu'
 function App() {
   return (
     <>
-      <h2>stripe submenus setup</h2>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Sidebar></Sidebar>
+      <Submenu></Submenu>
     </>
   )
 }
--- src/context.js
+++ src/context.js
@@ -1,2 +1,16 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value="Hello World">{children}</AppContext.Provider>
+  )
+}
+
+const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, useGlobalContext, AppProvider }
